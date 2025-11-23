fa93d762d36693c11a9d83c536fb422c69f852ba
gabrdsp <paulo.paiva@alunos.ufersa.edu.br>
2025-11-23 12:02:30 -0300

    Next updates.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
+const basePath = isProd && isGhPages ? '/Air' : '';
+const assetPrefix = isProd && isGhPages ? '/Air/' : '';
 
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-
+  basePath,
+  assetPrefix,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-
-  assetPrefix: isProd ? './' : '',
 };
 
 module.exports = nextConfig;
