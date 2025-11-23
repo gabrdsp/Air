1fe06965d039ab599dc63a0b40ae7d6e9abae72e
gabrdsp <paulo.paiva@alunos.ufersa.edu.br>
2025-11-22 23:33:28 -0300

    Ajusta assetPrefix para carregar CSS/JS no GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+
   images: {
     unoptimized: true,
   },
 
-  assetPrefix: isProd ? '/Air/' : '',
-
+  assetPrefix: isProd ? './' : '',
 };
 
 module.exports = nextConfig;
